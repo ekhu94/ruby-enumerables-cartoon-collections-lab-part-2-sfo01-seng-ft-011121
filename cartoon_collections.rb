360fe418f8050a2c20917c553a7810a5373430b7
Erik Huang <erikhuangdrums@gmail.com>
2021-01-04 21:40:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,3 +13,7 @@ end
 def find_valid_calls(arr)
   arr.find { |str| (str == str.capitalize) && (str[-1] == "!") }
 end
+
+mixed_calls = ["Cloud!", "earth", "Wind!", "cat", "Heart!"]
+
+puts find_valid_calls(mixed_calls)
